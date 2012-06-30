5b81f9109d2dc9c5562cda85d336779d17e5424f
Sun Ning <nsun@avos.com>
2012-06-30 16:59:21 +0800

    mass changes

--- src/timing/core.clj
+++ src/timing/core.clj
@@ -1,16 +1,30 @@
 (ns timing.core
   (:import [org.perf4j StopWatch LoggingStopWatch]))
 
-(defmacro defn-watched [name argvec & body]
+(defmacro defn-timed
+  "define a function which is timed by soul"
+  [name argvec & body]
   `(defn ~name argvec
      (let [watch# (LoggingStopWatch. ~name)
            result# (do ~@body)]
        (.stop watch#)
        result#)))
 
-(defn watched [func]
-  )
+(defn timed
+  "Wraps a function to be a timed function"
+  [tag func]
+  (fn [& args]
+    (let [watch (LoggingStopWatch. tag)
+          result (apply func args)]
+      (.stop watch)
+      result)))
 
-(defn wrap-watched [d]
-  )
+(defn wrap-timed
+  "An ring middleware to summarize calling time of each request"
+  [handler]
+  (fn [req]
+    (let [watch (LoggingStopWatch. (:uri req))]
+      (handler req)
+      (.stop watch))))
+
 
