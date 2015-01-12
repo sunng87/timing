e64217418360c3ff7ca42d83188182c99dd3d95e
Sun Ning <sunng@about.me>
2015-01-12 17:36:14 +0800

    bump version

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject timing "0.1.2"
+(defproject timing "0.1.3"
   :description "A simple timing library for clojure"
   :url "http://github.com/sunng87/timing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.perf4j/perf4j "0.9.16"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.perf4j/perf4j "0.9.16"]]
+  :deploy-repositories {"releases" :clojars})
