55e7560d66fb756329bda929d1771f736ffd5639
Sun Ning <sunng@about.me>
2015-03-23 14:47:47 +0800

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject timing "0.1.4-SNAPSHOT"
+(defproject timing "0.1.4"
   :description "A simple timing library for clojure"
   :url "http://github.com/sunng87/timing"
   :license {:name "Eclipse Public License"
